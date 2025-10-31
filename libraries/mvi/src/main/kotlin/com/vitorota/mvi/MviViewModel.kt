804af9abf9bec1883ce782490b313e0518d01533
vitorota <ota.vitor@gmail.com>
2025-10-31 10:42:10 -0300

    small refactors

--- libraries/mvi/src/main/kotlin/com/vitorota/mvi/MviViewModel.kt
+++ libraries/mvi/src/main/kotlin/com/vitorota/mvi/MviViewModel.kt
@@ -7,8 +7,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
